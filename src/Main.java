48c4defa6c698114e811e160fd80eaa881c2917d
hvy <hiroyuki.vincent.yamazaki@gmail.com>
2014-03-16 16:17:32 +0100

    Fixed a parsing bug

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,7 @@ import java.io.*;
 public class Main {
 
   // URL to the university data file
-  private static final String DATA_URL = "input/minikth";
+  private static final String DATA_URL = "../input/minikth";
 
   public static void main(String[] args) {
     GA ga = new GA();
